dd2c5936589db291e0114550f721ac7be3ebccc2
unknown <jonathan@ilustraviz.com>
2023-02-28 11:03:07 -0300

    entry page ajdusted, index page ajdustes, search adjusted, new page added, edit page added, random page added

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,15 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("css", views.css, name="css"),
-    path("django", views.django, name="Django"),
-    path("git", views.git, name="Git"),
-    path("html", views.html, name="HTML"),
-    path("python", views.python, name="Python"),
-    # path("<str:name>", views.pagenotfound, name="pagenotfound"),
+    path("", views.index, name="index"),    
+    path("library/<str:name>", views.library, name="library"),
     path("search", views.search, name="search"),
-    path("create", views.create, name="create")
+    path("create", views.create, name="create"),
+    path("library/<str:name>/edit", views.edit, name="edit"),
+    path("random", views.random_page, name="random")
     
     
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,67 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseRedirect, HttpResponseNotFound,HttpResponse
 import re
+import random
+
 
 from . import util
 
 
 def index(request):
+    
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
-def css(request):
-    return render(request, "encyclopedia/css.html", {
-        "texts": util.get_entry("CSS")
-    })
-    
-def django(request):
-    return render(request, "encyclopedia/django.html", {
-        "texts": util.get_entry("Django")
-    })
-
-def git(request):
-    return render(request, "encyclopedia/git.html", {
-        "texts": util.get_entry("Git")
-    })
-
-def html(request):
-    return render(request, "encyclopedia/html.html", {
-        "texts": util.get_entry("HTML")
-    })
-    
-def python(request):
-    return render(request, "encyclopedia/python.html", {
-        "texts": util.get_entry("Python")
-    })
-    
-# def pagenotfound(request, name):
-      
-#     return render(request, "encyclopedia/pagenotfound.html", {
-#         "name": name.capitalize()
-#     })
                    
-        
+def library(request, name):
+    
+    return render(request, "encyclopedia/library.html", {
+        "name":name ,"texts":util.get_entry(name)
+    })
+               
 def search(request):
+    
     if request.method == "POST":
         searched = request.POST["searched"]        
         r = re.compile(f".*{searched}")
-        newlist = list(filter(r.match, util.list_entries()))           
+        newlist = list(filter(r.match, util.list_entries())) #match similar names in the search        
         
         for entry in util.list_entries():
             if searched.lower() == entry.lower():
-                return HttpResponseRedirect(reverse(f"{entry}"))            
-        
+                return HttpResponseRedirect(reverse("library", kwargs={"name":entry})) #redirect dynamic url                     
+
         if newlist:          
             return render(request, "encyclopedia/search.html", {
                 "searchlist": newlist
             })  
-            
+                        
         else:
             return HttpResponseNotFound(f'<h1>Page {searched} not found</h1>')
         
-
-
 def create(request):
-    pass
+    if request.method == "POST":
+        title = request.POST["title"] 
+        content = request.POST["content"]
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse("index"))
+    return render(request, "encyclopedia/create.html")    
+
+def edit(request, name):    
+    if request.method == "POST":        
+        content = request.POST["content"]
+        util.save_entry(name, content)
+        return HttpResponseRedirect(reverse("index"))
+    
+    return render(request, "encyclopedia/edit.html", {
+        "name": name, "texts": util.get_entry(name)
+    })
+    
+def random_page(request):
+    entries = util.list_entries() # list of wikis
+    selected_page = random.choice(entries)
+    return HttpResponseRedirect(reverse("library", kwargs={"name":selected_page}))
